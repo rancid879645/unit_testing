7225c15ea1f13584c93cff4414af454cbc1517d6
Jonathan Velosa <jonathan_velosa@epam.com>
2023-12-06 09:53:42 -0500

    FirstPRoject to TDD

---
+++ CalcStats/Operations.cs
@@ -0,0 +1,49 @@
+﻿namespace CalcStats
+{
+    public class Operations
+    {
+        private readonly List<int> _numbers;
+
+        public Operations(string numbers)
+        {
+            _numbers = ConvertStringInListInt(numbers);
+        }
+
+        private static List<int> ConvertStringInListInt(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                throw new ArgumentNullException();
+
+            var result = new List<int>();
+
+            foreach (var number in numbers.Split())
+            {
+                if (!int.TryParse(number, out var validatedNumber))
+                    throw new InvalidCastException("you must digit only int numbers, character not allowed");
+                result.Add(validatedNumber);    
+            }
+            
+            return result;
+        }
+
+        public int GetMinValue()
+        {
+            return _numbers.Min();
+        }
+
+        public int GetMaxValue()
+        {
+            return _numbers.Max();
+        }
+
+        public int CountElements()
+        {
+            return _numbers.Count();
+        }
+
+        public float GetAverageValue()
+        {
+            return _numbers.Sum()/_numbers.Count();
+        }
+    }
+}
---
+++ CalcStats/Program.cs
@@ -0,0 +1,17 @@
+﻿using CalcStats;
+
+try
+{
+    Console.WriteLine("Please digit the number separate by - without spaces");
+    var numbers = Console.ReadLine();
+    var numberOperations = new Operations(numbers);
+    Console.WriteLine($"Min Value: {numberOperations.GetMinValue()}");
+    Console.WriteLine($"Max Value: {numberOperations.GetMaxValue()}");
+    Console.WriteLine($"Average: {numberOperations.GetAverageValue()}");
+    Console.WriteLine($"Count: {numberOperations.CountElements()}");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
+

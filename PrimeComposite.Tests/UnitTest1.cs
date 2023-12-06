9659e646d97f99dbcb3fc08277908cf95b765010
Jonathan Velosa <jonathan_velosa@epam.com>
2023-12-06 14:01:13 -0500

    First approasch to unit testing for PrimeComposite project

---
+++ PrimeComposite.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+namespace PrimeComposite.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Should_Throw_Invalid_Argument()
+        {
+        }
+
+        [TestMethod]
+        public void Should_Throw_Null_Argument()
+        {
+        }
+
+        [TestMethod]
+        public void Should_Throw_Invalid_Range()
+        {
+        }
+
+        [TestMethod]
+        public void Should_Print_Composite()
+        {
+        }
+
+        [TestMethod]
+        public void Should_Print_Prime()
+        {
+        }
+
+    }
+}
